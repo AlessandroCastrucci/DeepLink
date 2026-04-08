87a561605c4918ba4eb22bf22ef3f898884ee5f1
mar-031 <marina.mekail@digitalvirgo.com>
2026-04-08 17:17:42 +0200

    Updated deviceAuth.ts

--- src/api/deviceAuth.ts
+++ src/api/deviceAuth.ts
@@ -9,29 +9,10 @@ interface DeviceCodeResponse {
 interface DevicePollResponse {
   status: "pending" | "complete" | "expired";
   user?: {
-    code: string;
-    error: number;
-    data: {
-      "0": {
-        user_id: string;
-        email: string | null;
-        firstname?: string | null;
-        lastname?: string | null;
-        nickname?: string | null;
-        dve_login: string | null;
-        subscribed: boolean | null;
-        total_credit: string | null;
-        token?: Array<{
-          token: string;
-          content: string;
-          token_type: string;
-          immutable: boolean;
-          date_start: string;
-        }>;
-        offer?: Array<unknown>;
-        suspended?: boolean;
-      };
-    };
+    id: string;
+    email: string | null;
+    subscribed: boolean | null;
+    token: string;
   };
 }
 
