c0e2b78e4894ace78c7a910633409f44c5024675
mar-031 <marina.mekail@digitalvirgo.com>
2026-05-07 09:54:16 +0200

    Deleted banner.d.ts

--- packages/app-banner/dist/banner.d.ts
+++
@@ -1,3 +0,0 @@
-import type { AppBannerOptions } from "./config.js";
-export declare function mountBanner(options: AppBannerOptions): () => void;
-export declare function shouldShow(options: AppBannerOptions): boolean;
