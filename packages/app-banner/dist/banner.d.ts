d036c25fadf5a685f83d53b88c4a8ae66595d97e
mar-031 <marina.mekail@digitalvirgo.com>
2026-05-07 09:54:17 +0200

    Added index.d.ts

---
+++ packages/app-banner/dist/banner.d.ts
@@ -0,0 +1,3 @@
+import type { AppBannerOptions } from "./config.js";
+export declare function mountBanner(options: AppBannerOptions): () => void;
+export declare function shouldShow(options: AppBannerOptions): boolean;
